4de2ac3556f2ac735c82e2d0914d1e3fdcab236a
desenvolvedorback <desenvolvimentobackend.com@gmail.com>
2025-10-27 06:28:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -265,6 +265,6 @@ def admin_logout():
 
 # ---------------- Run ----------------
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render define a porta automaticamente
+    app.run(host="0.0.0.0", port=port)
